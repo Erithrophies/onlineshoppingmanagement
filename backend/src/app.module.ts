b6ea8416fd439985d8332477cd3afe17bccea4f1
Erithrophies <111172386+Erithrophies@users.noreply.github.com>
2025-07-23 13:41:24 +0600

    orm

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,19 @@ import { Module } from '@nestjs/common';
 import { AdminModule } from './admin/admin.module';
 import { UserModule } from './user/user.module';
 import { SellerModule } from './seller/seller.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ AdminModule, UserModule, SellerModule],
+  imports: [ AdminModule, UserModule, SellerModule, TypeOrmModule.forRoot(
+{ type: 'postgres',
+host: 'localhost',
+port: 5432,
+username: 'postgres',
+password: '1234',
+database: 'osm',//Change to your database name
+autoLoadEntities: true,
+synchronize: true,
+} )],
   controllers: [],
   providers: [],
 })
--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { UserEntity } from "./user.entity";
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 
 @Module({
-    imports: [UserModule],
+    imports: [UserModule, TypeOrmModule.forFeature([UserEntity]),],
     controllers: [UserController],
     providers: [UserService],
 })
